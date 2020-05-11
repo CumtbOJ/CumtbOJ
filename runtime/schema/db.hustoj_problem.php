e8bdeee65a1725f92d3a82de061fb37c50bb5f06
mqd <1045723308@qq.com>
2020-05-11 11:44:07 +0800

    1

---
+++ runtime/schema/db.hustoj_problem.php
@@ -0,0 +1,11 @@
+<?php 
+return array (
+  'status' => 'string',
+  'id' => 'int',
+  'name' => 'string',
+  'index' => 'string',
+  'level' => 'string',
+  'passrate' => 'float',
+  '_pk' => 'id',
+  '_autoinc' => 'id',
+);
