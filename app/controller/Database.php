a1b8125f453c85c2f781924ecc1183848e3d1fe3
mqd <1045723308@qq.com>
2020-05-11 11:44:59 +0800

    heihie

--- app/controller/Database.php
+++ app/controller/Database.php
@@ -3,17 +3,7 @@ namespace app\controller;
 use app\model\hustoj_problem as Hp;
 use app\model\hustoj_users as Hu;
 class Database{
-    public function index(){
-        $user = Hp::where('id','1')->find();
-        $user -> status = 2;
-        $user -> index = "123";
-        $user -> save();
-        return json($user);
-    }
-    public function index2(){
-        $user = Hu::where('username','t1')->find();
-        $user-> status = 1;
-        $user -> save() ;
-        return json($user);
+    public function showUser(){
+        Db::name("hustoj_users");
     }
 }
