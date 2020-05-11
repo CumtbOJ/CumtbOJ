3115727a27b568163780470fd80385a25dda8efd
mqd <1045723308@qq.com>
2020-05-11 10:26:21 +0800

    清除了缓存 runtime/schema/db.hustoj_users.php 不然用users表会报错

---
+++ runtime/schema/db.hustoj_users.php
@@ -0,0 +1,11 @@
+<?php 
+return array (
+  'nick' => 'string',
+  'id' => 'int',
+  'username' => 'string',
+  'password' => 'string',
+  'status' => 'string',
+  'ss' => 'string',
+  '_pk' => 'id',
+  '_autoinc' => 'id',
+);
