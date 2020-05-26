a1b7a29f6b5f107da3c88d7f300ee38b56ec7db0
mqd <1045723308@qq.com>
2020-05-26 16:06:39 +0800

    数据库表改了

--- data/mysql/hustoj_access.sql
+++ data/mysql/hustoj_access.sql
@@ -7,6 +7,6 @@ CREATE TABLE `hustoj_access` (
   `uid` int unsigned NOT NULL,
   `pid` int unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1102 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1102 DEFAULT CHARSET=utf8mb4 ;
 
 SET FOREIGN_KEY_CHECKS = 1;
